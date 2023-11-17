dcf1be02c695689944fbdd321e3ae2e20a441a3e
Daniel Gonzales <danielsmart5@hotmail.com>
2023-11-17 13:35:02 -0500

    Starting

---
+++ src/test/java/com/example/helpdesk2/HelpDesk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helpdesk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDesk2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
